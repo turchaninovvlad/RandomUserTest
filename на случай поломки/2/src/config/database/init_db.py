783aeaba0602d4a8f8e95a1e6dba18f4443df257
turchaninovvlad <turchaninov-vlad@mail.ru>
2025-05-24 15:34:17 +0300

    1

--- на случай поломки/2/src/config/database/init_db.py
+++
@@ -1,9 +0,0 @@
-from contextlib import asynccontextmanager
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlmodel import SQLModel
-from .db_helper import engine, async_session_maker
-
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
-
